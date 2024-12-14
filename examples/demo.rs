4e714a9236e7c2dc02decd54e34c5bc1efa6b1c2
Elmar Athmer <elmar@athmer.org>
2024-12-14 15:38:11 +0100

    run two servers in demo

--- examples/demo.rs
+++ examples/demo.rs
@@ -1,15 +1,31 @@
 use axum::routing::get;
 use axum::{Json, Router};
 use serde_json::json;
+use std::error::Error;
 
 #[tokio::main]
-async fn main() {
-    let app = Router::new().route("/", get(|| async { Json(json!({ "msg": "Hello World" })) }));
+async fn main() -> Result<(), Box<dyn Error>> {
+    let app1 = Router::new().route(
+        "/api",
+        get(|| async { Json(json!({ "msg": "A message" })) }),
+    );
 
-    let listener = tokio::net::TcpListener::bind("127.0.0.1:3000")
-        .await
-        .unwrap();
+    let app2 = Router::new().route(
+        "/api",
+        get(|| async { Json(json!({ "msg": "Another message" })) }),
+    );
 
-    println!("listening on {}", listener.local_addr().unwrap());
-    axum::serve(listener, app).await.unwrap();
+    let listener1 = tokio::net::TcpListener::bind("127.0.0.1:3000").await?;
+    let listener2 = tokio::net::TcpListener::bind("127.0.0.1:3001").await?;
+
+    println!(
+        "listening on {} and {}",
+        listener1.local_addr()?,
+        listener2.local_addr()?
+    );
+
+    tokio::spawn(async move { axum::serve(listener1, app1).await });
+    axum::serve(listener2, app2).await?;
+
+    Ok(())
 }
