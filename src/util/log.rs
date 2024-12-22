57fbdcc4295b5e551ebc5d3328796e18b584c5f8
Elmar Athmer <elmar@athmer.org>
2024-12-22 22:06:54 +0100

    introduce basic config
    fixes #5

--- src/util/log.rs
+++ src/util/log.rs
@@ -1,12 +1,7 @@
-use lenient_bool::LenientBool;
-
 /// initialize the tracing subscriber.
 ///
 /// by default, it uses human-readable logs, unless the env-variable `LOG_JSON` is set to a `truethy` value, then it emits json-formatted-logs
-pub fn init() {
-    let log_json = std::env::var("LOG_JSON").map(|s| s.parse::<LenientBool>().unwrap_or_default())
-        == Ok(LenientBool(true));
-
+pub fn init(log_json: bool) {
     if log_json {
         tracing_subscriber::fmt()
             .json()
