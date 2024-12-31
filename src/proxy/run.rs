fa3694d5869e266e343c2117fc3eba7c02a6a844
Elmar Athmer <elmar@athmer.org>
2024-12-31 15:35:26 +0100

    start multiple threads listening to connections

--- src/proxy/run.rs
+++ src/proxy/run.rs
@@ -5,6 +5,7 @@ use hyper_util::rt::TokioIo;
 use hyper_util::service::TowerToHyperService;
 use std::net::SocketAddr;
 use std::sync::Arc;
+use std::thread;
 use tokio::net::TcpListener;
 use tower::ServiceBuilder;
 use tracing::error;
@@ -14,7 +15,23 @@ pub async fn run(port: u16, proxy: proxy::Service) -> tokio::io::Result<()> {
     let proxy = Arc::new(proxy);
 
     let addr = SocketAddr::from(([0, 0, 0, 0], port));
-    let listener = TcpListener::bind(addr).await?;
+
+    // Spawn a thread for each available core, minus one, since we'll
+    // reuse the main thread as a server thread as well.
+    for _ in 1..num_cpus::get() {
+        let proxy = proxy.clone();
+        let addr = addr.clone();
+        thread::spawn(move || async move {
+            server_thread(addr, proxy).await.unwrap();
+        });
+    }
+
+    server_thread(addr, proxy).await
+}
+
+pub async fn server_thread(addr: SocketAddr, proxy: Arc<proxy::Service>) -> tokio::io::Result<()> {
+    // let addr = SocketAddr::from(([0, 0, 0, 0], port));
+    let listener = reuse_listener(&addr)?;
 
     let trace_layer = proxy::log::new_trace_layer();
 
@@ -43,4 +60,25 @@ pub async fn run(port: u16, proxy: proxy::Service) -> tokio::io::Result<()> {
             }
         });
     }
+}
+
+fn reuse_listener(addr: &SocketAddr) -> std::io::Result<TcpListener> {
+    let builder = match *addr {
+        SocketAddr::V4(_) => net2::TcpBuilder::new_v4()?,
+        SocketAddr::V6(_) => net2::TcpBuilder::new_v6()?,
+    };
+
+    #[cfg(unix)]
+    {
+        use net2::unix::UnixTcpBuilderExt;
+        if let Err(e) = builder.reuse_port(true) {
+            error!("error setting SO_REUSEPORT: {}", e);
+        }
+    }
+
+    builder.reuse_address(true)?;
+    builder.bind(addr)?;
+    let listener = builder.listen(1024)?;
+    listener.set_nonblocking(true)?;
+    TcpListener::from_std(listener)
 }
