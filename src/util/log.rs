813148faa47f23472cee6a118f3e5308af690538
Elmar Athmer <elmar@athmer.org>
2024-12-31 12:18:25 +0100

    enable log-level configuration via RUST_LOG

--- src/util/log.rs
+++ src/util/log.rs
@@ -1,3 +1,5 @@
+use tracing_subscriber::filter::{EnvFilter, LevelFilter};
+
 /// initialize the tracing subscriber.
 ///
 /// by default, it uses human-readable logs, unless the env-variable `LOG_JSON` is set to a `truethy` value, then it emits json-formatted-logs
@@ -5,12 +7,20 @@ pub fn init(log_json: bool) {
     if log_json {
         tracing_subscriber::fmt()
             .json()
-            .with_max_level(tracing::Level::INFO)
+            .with_env_filter(
+                EnvFilter::builder()
+                    .with_default_directive(LevelFilter::INFO.into())
+                    .from_env_lossy(),
+            )
             .init();
     } else {
         tracing_subscriber::fmt()
             .pretty()
-            .with_max_level(tracing::Level::INFO)
+            .with_env_filter(
+                EnvFilter::builder()
+                    .with_default_directive(LevelFilter::INFO.into())
+                    .from_env_lossy(),
+            )
             .init();
     };
 }
