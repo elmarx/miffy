a2b91415d24594b0d317d32f7c902badb7bb73c1
Elmar Athmer <elmar@athmer.org>
2025-03-04 11:42:49 +0100

    support stackdriver logging
    fixes #53

--- src/util/log.rs
+++ src/util/log.rs
@@ -1,5 +1,7 @@
 use serde::Deserialize;
 use tracing_subscriber::filter::{EnvFilter, LevelFilter};
+use tracing_subscriber::layer::SubscriberExt;
+use tracing_subscriber::util::SubscriberInitExt;
 
 #[derive(Deserialize, Debug)]
 #[serde(rename_all = "lowercase")]
@@ -8,26 +10,32 @@ pub enum Format {
     Json,
     /// log in pretty, human-readable format
     Human,
+
+    /// google cloud compatible structured logging: <https://cloud.google.com/logging/docs/structured-logging />
+    Stackdriver,
 }
 
 /// initialize the tracing subscriber.
 pub fn init(format: &Format) {
+    let env_filter = EnvFilter::builder()
+        .with_default_directive(LevelFilter::INFO.into())
+        .from_env_lossy();
+
     match format {
         Format::Json => tracing_subscriber::fmt()
             .json()
-            .with_env_filter(
-                EnvFilter::builder()
-                    .with_default_directive(LevelFilter::INFO.into())
-                    .from_env_lossy(),
-            )
+            .with_env_filter(env_filter)
             .init(),
         Format::Human => tracing_subscriber::fmt()
             .pretty()
-            .with_env_filter(
-                EnvFilter::builder()
-                    .with_default_directive(LevelFilter::INFO.into())
-                    .from_env_lossy(),
-            )
+            .with_env_filter(env_filter)
             .init(),
+        Format::Stackdriver => {
+            let stackdriver = tracing_stackdriver::layer();
+            tracing_subscriber::registry()
+                .with(stackdriver)
+                .with(env_filter)
+                .init()
+        }
     }
 }
